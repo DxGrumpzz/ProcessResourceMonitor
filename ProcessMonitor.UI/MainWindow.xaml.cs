afa6c80604d7ddaca22470805dad150944e4734a
Josef Lintz <yosi1.lints@gmail.com>
2020-09-15 17:37:23 +0300

    * Changed constructor argument in MainWindow code behind

--- ProcessMonitor.UI/MainWindow.xaml.cs
+++ ProcessMonitor.UI/MainWindow.xaml.cs
@@ -1,35 +1,23 @@
 ﻿namespace ProcessMonitor.UI
 {
     using System.Windows;
-    using System.Windows.Controls;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        ProcessData[] _processList;
-
+       
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public MainWindow(ProcessData[] processData)
+        public MainWindow(MainWindowViewModel viewModel)
         {
-            _processList = processData;
             InitializeComponent();
+            DataContext = viewModel;
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            foreach (var process in _processList)
-            {
-                MainStackPanel.Children.Add(new TextBlock()
-                {
-                    Text = process.ProcessName,
-                });
-            };
-        }
     };
 };
