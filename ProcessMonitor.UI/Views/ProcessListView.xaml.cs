69f7388eeda7250f82c472729fa3a5579948653e
Josef Lintz <yosi1.lints@gmail.com>
2020-09-15 17:32:58 +0300

    + Added ProcessList view

---
+++ ProcessMonitor.UI/Views/ProcessListView.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace ProcessMonitor.UI
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ProcessListView.xaml
+    /// </summary>
+    public partial class ProcessListView : UserControl
+    {
+
+        public ProcessListView()
+        {
+            InitializeComponent();
+        }
+
+        public ProcessListView(ProcessListViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+
+    };
+};
