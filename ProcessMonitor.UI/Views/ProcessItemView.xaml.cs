a277667f2bfd6aab5e01165774f77397a004fe27
Josef Lintz <yosi1.lints@gmail.com>
2020-09-15 17:32:47 +0300

    + Added ProcessItem view

---
+++ ProcessMonitor.UI/Views/ProcessItemView.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace ProcessMonitor.UI
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ProcessItemView.xaml
+    /// </summary>
+    public partial class ProcessItemView : UserControl
+    {
+
+        public ProcessItemView(ProcessItemViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+
+        public ProcessItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
