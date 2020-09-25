c237135d7c280472e81e12741c05c8f2142921a3
Josef Lintz <yosi1.lints@gmail.com>
2020-09-25 19:19:55 +0300

    + Added 'IMainViewChanger' interface and implementation

---
+++ ProcessMonitor.UI/Interfaces/IMainViewChanger.cs
@@ -0,0 +1,23 @@
+﻿namespace ProcessMonitor.UI
+{
+    /// <summary>
+    /// An interface for a view changer
+    /// </summary>
+    /// <typeparam name="TViewKey"></typeparam>
+    public interface IMainViewChanger<TViewKey>
+    {
+        /// <summary>
+        /// Change the main application view
+        /// </summary>
+        /// <param name="viewKey"> A view key that will be used to find the correct view </param>
+        public void ChangeView(TViewKey viewKey);
+
+        /// <summary>
+        /// Change the main application view
+        /// </summary>
+        /// <typeparam name="T"> The viewmodel type </typeparam>
+        /// <param name="viewKey"> A view key that will be used to find the correct view </param>
+        /// <param name="viewArguments"> arguments that can be passed to the view </param>
+        public void ChangeView<T>(TViewKey viewKey, T viewArguments);
+    };
+};
---
+++ ProcessMonitor.UI/UI/ViewChangers/WPFMainViewChanger.cs
@@ -0,0 +1,62 @@
+﻿namespace ProcessMonitor.UI
+{
+    using System.Collections.Generic;
+    using System.Windows.Controls;
+    
+    /// <summary>
+    /// An enumaration of possible main views
+    /// </summary>
+    public enum MainViews
+    {
+        ProcessListView = 0,
+        ProcessItemView = 1,
+        View2 = 2,
+        View3 = 3,
+    }
+
+
+    /// <summary>
+    /// An implementation of <see cref="IMainViewChanger{TViewKey}"/> for WPF
+    /// </summary>
+    /// <typeparam name="TViewKey"></typeparam>
+    public class WPFMainViewChanger<TViewKey> : IMainViewChanger<TViewKey>
+    {
+        /// <summary>
+        /// A dictionary containing view keys and views
+        /// </summary>
+        private Dictionary<TViewKey, UserControl> _views;
+
+        /// <summary>
+        /// The MainViewmodel model. This is here because the "main view" is stored there
+        /// </summary>
+        private MainWindowViewModel _mainWindowViewModel;
+
+        public WPFMainViewChanger(MainWindowViewModel mainWindowViewModel, Dictionary<TViewKey, UserControl> views)
+        {
+            _mainWindowViewModel = mainWindowViewModel;
+            _views = views;
+        }
+
+        public void ChangeView(TViewKey viewKey)
+        {
+            // Find the view 
+            var view = _views[viewKey];
+
+            // Change the view
+            _mainWindowViewModel.CurrentMainView = view;
+        }
+
+        public void ChangeView<T>(TViewKey viewKey, T viewmodel)
+        {
+            // Find the view 
+            var view = _views[viewKey];
+
+            // For now only change the data context of the view to the given T viewmodel
+            view.DataContext = viewmodel;
+
+            // Update the view
+            _mainWindowViewModel.CurrentMainView = view;
+        }
+
+    }
+};
